0da0c4c817911f4b15ff4c8d8ff80d47cd7cc36f
Guilherme Abreu <guilherme.abreu378@gmail.com>
2023-09-05 01:08:03 +0000

    Aula 04/09

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask("meu app")
 
+posts = [
+    {
+        "titulo": "Minha Primeira Postagem",
+        "texto": "Texto do meu primeiro post"
+    },
+    {
+        "titulo": "Minha Segunda Postagem",
+        "texto": "Texto do meu segundo post"
+    }
+]
+
 @app.route('/')
-def hello():
-    return "Hello World"
+def exibir_entradas():
+    entradas = posts
+    return render_template('exibir_entradas.html', entradas=entradas)
